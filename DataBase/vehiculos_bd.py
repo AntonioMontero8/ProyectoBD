3156b3251f1006c3085461dc3e3cf085b66f8e9a
AntonioMontero8 <josantomontero@gmail.com>
2026-05-08 16:57:17 -0600

    Commit de lo funcional hasta el momento

---
+++ DataBase/vehiculos_bd.py
@@ -0,0 +1,71 @@
+import mysql.connector
+import utils.conexion_BD as conn
+from utils.entidades import Vehiculo
+import utils.entidades as entidades
+
+class vehiculos_bd:
+    def buscar(self, matricula):
+        self.con = conn.conection().connect()
+        self.cursor1 = self.con.cursor()
+        sql = "SELECT matricula, modelo, marca, color, cliente_id FROM vehiculos WHERE matricula = %s "
+        self.cursor1.execute(sql, (matricula,))
+        resultado = self.cursor1.fetchone()
+        self.con.close()     
+        if resultado:
+            return resultado
+        else:
+            return False
+        
+    def guardar(self,vehiculo:Vehiculo):
+        try:
+            self.con = conn.conection().connect()
+            self.cursor1 = self.con.cursor()
+            sql = """
+            INSERT INTO vehiculos
+            (matricula, modelo, marca, color, cliente_id)
+            VALUES (%s, %s, %s, %s, %s)
+            """
+            valores = (
+                vehiculo.get_matricula(),
+                vehiculo.get_modelo(),
+                vehiculo.get_marca(),
+                vehiculo.get_color(),
+                vehiculo.get_cliente_id()
+            )
+            self.cursor1.execute(sql,valores)
+            self.con.commit()
+            self.con.close() 
+            return True, "Vehículo guardado correctamente"
+        
+        except mysql.connector.Error as e:
+            if e.errno == 1062:
+                return False, "La matrícula ya existe"
+
+            elif e.errno == 1452:
+                return False, "El cliente_id no existe"
+
+            return False, f"Error MariaDB: {e}"
+        finally:
+            if hasattr(self, 'con') and self.con.is_connected():
+                self.con.close()
+        
+        
+    def BuscarPorCliente(self, Cliente):
+        self.con = conn.conection().connect()
+        self.cursor1 = self.con.cursor()
+        sql = "SELECT matricula, marca, modelo, color, cliente_id FROM vehiculos WHERE cliente_id = %s"
+        self.cursor1.execute(sql, (Cliente.get_cliente_id(),))
+        resultados = self.cursor1.fetchall()
+        
+        lista_vehiculos = []
+        for res in resultados:
+            v = entidades.Vehiculo()
+            v.set_matricula(res[0])
+            v.set_marca(res[1])
+            v.set_modelo(res[2])
+            v.set_color(res[3])
+            v.set_cliente_id(res[4])
+            lista_vehiculos.append(v)
+            
+        self.con.close()
+        return lista_vehiculos
---
+++ views/login.py
@@ -0,0 +1,98 @@
+import customtkinter as ctk
+import myCustomTkinter as mctk
+import utils.entidades as entidades
+from DataBase.usuario_bd import usuario_bd
+from utils.helpers import limpiar_ventana
+
+# Definición de variables globales para evitar advertencias de alcance (scope)
+id_usuario_logueado = None
+perfil = None
+
+def revisar_credenciales(entry_u, entry_p):
+    global id_usuario_logueado, perfil
+    nombre_usuario = entry_u.get()
+    password = entry_p.get()
+    usuario = entidades.Usuario()
+    usuario.set_username(nombre_usuario)
+    usuario.set_password(password)
+    db = usuario_bd()
+    resultado = db.HacerLogin(usuario)
+    if resultado:
+        id_usuario_logueado, perfil, nombre_usuario = resultado
+        mostrar_popup(nombre_usuario, True)
+        print(f"Intento de login - ID_Usuario: {id_usuario_logueado}, Perfil: {perfil}, Usuario: {nombre_usuario}, Password: {password}")
+    else:
+        mostrar_popup(None, False)
+    
+def mostrar_popup(nombre_usuario, ingresado):
+    if ingresado:
+        mensaje = f"Sesión iniciada: {nombre_usuario}"
+    else:
+        mensaje = "No se pudo autenticar este usuario"
+    popup = ctk.CTkToplevel()
+    popup.title("Inicio de Sesión")
+
+    popup.geometry("350x150")
+
+    popup.grab_set() #No puede interactuar con el fondo  hasta cerrar el pop-up
+    popup.attributes("-topmost", True)
+
+    etiqueta = ctk.CTkLabel(popup, text=mensaje, font=("Roboto", 16, "bold"))
+    etiqueta.pack(pady=(30, 20))
+
+    boton_aceptar = ctk.CTkButton(popup, text="Aceptar", command=popup.destroy)
+    boton_aceptar.pack()    
+    
+
+def pantalla_login(ventana):
+
+    limpiar_ventana(ventana)
+
+    ctk.CTkLabel(
+        ventana,
+        text="Acceso al Sistema",
+        font=("Arial", 28, "bold")
+    ).pack(pady=(50, 20))
+
+    frame_form = ctk.CTkFrame(
+        ventana,
+        width=400,
+        height=300
+    )
+
+    frame_form.pack(pady=20, padx=60)
+    frame_form.pack_propagate(False)
+
+    ctk.CTkLabel(
+        frame_form,
+        text="Usuario:",
+        font=("Arial", 14)
+    ).pack(pady=(30, 5))
+
+    # Asignamos el Entry a una variable
+    entry_usuario = ctk.CTkEntry(
+        frame_form,
+        placeholder_text="Admin / Cobrador",
+        width=250
+    )
+    entry_usuario.pack(pady=5)
+
+    ctk.CTkLabel(
+        frame_form,
+        text="Contraseña:",
+        font=("Arial", 14)
+    ).pack(pady=(15, 5))
+
+    entry_password = mctk.PasswordEntry(
+        frame_form,
+        placeholder_text="********",
+        width=250
+    )
+    entry_password.pack(pady=5)
+
+    ctk.CTkButton(
+        frame_form,
+        text="Iniciar Sesión",
+        width=200,
+        command=lambda: revisar_credenciales(entry_usuario, entry_password)
+    ).pack(pady=30)
